320e05f761d39bf144ef8cc39df353442cc4389e
Numfor C. Tiapo <numsmt@yahoo.com>
2016-08-22 20:37:25 -0400

    adding migrations

--- db/migrate/20160823003054_create_schools.rb
+++ db/migrate/20160823003054_create_schools.rb
@@ -1,6 +1,14 @@
 class CreateSchools < ActiveRecord::Migration
   def change
     create_table :schools do |t|
+    	t.string :name
+    	t.string :image
+    	t.integer :division
+    	t.string :proximity
+    	t.integer :population
+    	t.integer :computer_science_rank
+    	t.integer :tuition
+    	t.integer :act_score
 
       t.timestamps null: false
     end
--- db/migrate/20160823003123_create_facts.rb
+++ db/migrate/20160823003123_create_facts.rb
@@ -1,6 +1,7 @@
 class CreateFacts < ActiveRecord::Migration
   def change
     create_table :facts do |t|
+    	t.string :description
 
       t.timestamps null: false
     end
